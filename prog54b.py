c95a049bcc8646b7cc3f1c574164c7f73b069a7c
timothy-2005 <69025702+timothy-2005@users.noreply.github.com>
2022-09-09 19:32:38 +0000

    .

--- prog54b.py
+++ prog54b.py
@@ -1,7 +1,7 @@
-a = 475
-b = 821
-c = 369
-d = 562
+a = int(input("please enter four numbers\n1:"))
+b = int(input("2:"))
+c = int(input("3:"))
+d = int(input("4:"))
 sum = a + b + c + d
 ave = sum / 4
 print("The sum of the four numbers is", sum)
