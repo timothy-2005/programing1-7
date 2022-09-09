3896d87603ce4308be3f76f199a18391ee844ce1
timothy-2005 <69025702+timothy-2005@users.noreply.github.com>
2022-09-09 19:20:18 +0000

    9.9 in class

---
+++ prog52a.py
@@ -0,0 +1,8 @@
+length = int(input("length:"))
+width = int(input("width:"))
+area = length * width
+perimeter = 2 * (length + width)
+print("\nThe Length is", length)
+print("The Width is", width)
+print("\nthe area is", area)
+print("the perimeter is", perimeter)
